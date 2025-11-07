c6b42298ad1ae67ea5cc31673988bbf744cdfdc0
Molnár Ákos <mabphun@gmail.com>
2025-11-07 23:27:50 +0100

    Backend init

---
+++ IDS-backend/src/main/java/com/mabphun/IDS_backend/IdsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mabphun.IDS_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsBackendApplication.class, args);
+	}
+
+}
---
+++ IDS-backend/src/test/java/com/mabphun/IDS_backend/IdsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabphun.IDS_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdsBackendApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
