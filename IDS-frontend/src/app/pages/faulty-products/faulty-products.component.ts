8f1a99701f7d1f756d7276a3f5a8996ffbb1cdd2
Molnár Ákos <mabphun@gmail.com>
2025-11-11 01:03:45 +0100

    Added new pages and navigation

---
+++ IDS-frontend/src/app/pages/faulty-products/faulty-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faulty-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './faulty-products.component.html',
+  styleUrl: './faulty-products.component.scss'
+})
+export class FaultyProductsComponent {
+
+}
